9a96305dff8dd290604c91d19891db7f8180c71a
Kuntal <theencodedbong@gmail.com>
2021-08-18 23:27:04 +0530

    Initial uploads

---
+++ Jenkinsfile
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    tools {
+    	maven 'Maven 3.5.4'
+    	jdk 'Java 13'
+    }
+    stages {
+        stage('Initialize') {
+            steps {
+                echo "PATH: ${PATH}"
+                echo "MAVEN: ${M2_HOME}"
+            }
+        }
+        stage('Build') {
+            steps {
+            	echo "Start of build..."
+                sh 'mvn clean install'
+            }
+        }
+    }
+}
---
+++ src/main/java/com/teb/practice/restfulapi101/controller/BookController.java
@@ -0,0 +1,67 @@
+package com.teb.practice.restfulapi101.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teb.practice.restfulapi101.bean.Book;
+import com.teb.practice.restfulapi101.exception.BookNotFoundException;
+import com.teb.practice.restfulapi101.service.BookService;
+
+@RestController
+public class BookController {
+
+	@Autowired
+	BookService bookService;
+
+	@GetMapping("/")
+	public String welcomeMessage() {
+		return "Hello!";
+	}
+
+	@GetMapping("/books")
+	public List<Book> getBookList() {
+		return bookService.listBooks();
+	}
+
+	@GetMapping("/books/{bookId}")
+	public Book getBookById(@PathVariable String bookId) {
+		Book bookToLookup = bookService.listBookById(bookId);
+		if (bookToLookup == null) {
+			throw new BookNotFoundException("Book ID: ".concat(bookId.toUpperCase()));
+		}
+		return bookToLookup;
+	}
+
+	@PostMapping("/books")
+	public Book addNewBook(@Valid @RequestBody Book book) {
+		return bookService.addBook(book);
+	}
+
+	@PatchMapping("/books/{bookId}")
+	public Book updateOldBook(@PathVariable String bookId, @RequestBody Book book) {
+		Book bookToUpdate = bookService.updateBook(bookId, book);
+		if (bookToUpdate == null) {
+			throw new BookNotFoundException("Book ID: ".concat(bookId.toUpperCase()));
+		}
+		return bookToUpdate;
+	}
+
+	@DeleteMapping("/books/{bookId}")
+	public Book deleteOldBook(@PathVariable String bookId) {
+		Book bookToDelete = bookService.removeBook(bookId);
+		if (bookToDelete == null) {
+			throw new BookNotFoundException("Book ID: ".concat(bookId.toUpperCase()));
+		}
+		return bookToDelete;
+	}
+}
